aad5ac114a34e5e2fcc5bfcd10ce6abec912e8f1
mury7 <cipriancluj1976@gmail.com>
2018-06-08 15:51:06 +0300

    added test for register user with duplcated email.

--- src/test/java/org/fasttrackit/search/SearchTest.java
+++ src/test/java/org/fasttrackit/search/SearchTest.java
@@ -16,20 +16,35 @@ public class SearchTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://fasttrackit.org/selenium-test/");
 
+        String email = "mail" + System.currentTimeMillis() + "@mail.com";
+
+        registerUser(driver, email);
+        driver.findElement(By.className("success-msg"));
+        WebElement successMsgContainer = driver.findElement(By.className("success-msg"));
+        assertThat("Success message not displayed", successMsgContainer.isDisplayed());
+        driver.quit();
+
+        driver = new ChromeDriver();
+        driver.get("https://fasttrackit.org/selenium-test/");
+        registerUser(driver, email);
+        driver.findElement(By.className("error-msg"));
+        WebElement errorMsgContainer = driver.findElement(By.className("error-msg"));
+        assertThat("Error message not displayed", errorMsgContainer.isDisplayed());
+        driver.quit();
+
+    }
+
+    private void registerUser(WebDriver driver, String email) {
         driver.findElement(By.linkText("ACCOUNT")).click();
         driver.findElement(By.linkText("Register")).click();
         driver.findElement(By.id("firstname")).sendKeys("First");
         driver.findElement(By.id("middlename")).sendKeys("Automation");
         driver.findElement(By.id("lastname")).sendKeys("TestAttempt");
-        driver.findElement(By.id("email_address")).sendKeys("mail@mail.com");
+        driver.findElement(By.id("email_address")).sendKeys(email);
         driver.findElement(By.id("password")).sendKeys("123456");
         driver.findElement(By.id("confirmation")).sendKeys("123456");
         driver.findElement(By.name("is_subscribed")).click();
         driver.findElement(By.cssSelector("button[title='Register']")).click();
-        driver.findElement(By.className("error-msg"));
-        WebElement errorMsgContainer = driver.findElement(By.className("error-msg"));
-        assertThat("Error message not displayed", errorMsgContainer.isDisplayed());
 
-//      driver.quit();
     }
 }
