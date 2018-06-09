e7c630ed8d2a80c2b8f47d11b48f4d640b536e25
mury76 <cipriancluj1976@gmail.com>
2018-06-09 14:57:43 +0300

    added login test

---
+++ src/test/java/org/fasttrackit/search/LoginTest.java
@@ -0,0 +1,33 @@
+package org.fasttrackit.search;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class LoginTest {
+
+    @Test
+    public void loginTest(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\mury7\\IdeaProjects\\madisonIslandTestByMry\\src\\test\\resources\\drivers\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver(); {
+
+            driver.get("https://fasttrackit.org/selenium-test/");
+            driver.findElement(By.linkText("ACCOUNT")).click();
+            driver.findElement(By.linkText("Log In")).click();
+            driver.findElement(By.name("login[username]")).sendKeys("mail@mail.com");
+            driver.findElement(By.name("login[password]")).sendKeys("123456");
+            driver.findElement(By.cssSelector("button[title='Login']")).click();
+            driver.findElement(By.className("welcome-msg"));
+            WebElement errorMsgContainer = driver.findElement(By.className("welcome-msg"));
+            assertThat("The user is not logged in", errorMsgContainer.isDisplayed());
+            driver.quit();
+        }
+
+    }
+
+}
--- src/test/java/org/fasttrackit/search/RegisterTest.java
+++ src/test/java/org/fasttrackit/search/RegisterTest.java
@@ -8,22 +8,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class SearchTest {
+public class RegisterTest {
 
     @Test
     public void registerTest(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\mury7\\FastTrackIt\\nadisonIslandTest\\src\\test\\resources\\drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://fasttrackit.org/selenium-test/");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\mury7\\IdeaProjects\\madisonIslandTestByMry\\src\\test\\resources\\drivers\\chromedriver.exe");
 
+        // Creating unique e-mail.
         String email = "mail" + System.currentTimeMillis() + "@mail.com";
 
+        // Verifying successful register with valid data.
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://fasttrackit.org/selenium-test/");
         registerUser(driver, email);
         driver.findElement(By.className("success-msg"));
         WebElement successMsgContainer = driver.findElement(By.className("success-msg"));
         assertThat("Success message not displayed", successMsgContainer.isDisplayed());
         driver.quit();
 
+        // Verifying error message if user enter an existing e-mail when register.
         driver = new ChromeDriver();
         driver.get("https://fasttrackit.org/selenium-test/");
         registerUser(driver, email);
@@ -31,9 +34,9 @@ public class SearchTest {
         WebElement errorMsgContainer = driver.findElement(By.className("error-msg"));
         assertThat("Error message not displayed", errorMsgContainer.isDisplayed());
         driver.quit();
-
     }
 
+    // Completing register form.
     private void registerUser(WebDriver driver, String email) {
         driver.findElement(By.linkText("ACCOUNT")).click();
         driver.findElement(By.linkText("Register")).click();
@@ -45,6 +48,5 @@ public class SearchTest {
         driver.findElement(By.id("confirmation")).sendKeys("123456");
         driver.findElement(By.name("is_subscribed")).click();
         driver.findElement(By.cssSelector("button[title='Register']")).click();
-
     }
 }
