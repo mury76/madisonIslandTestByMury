8ea71006b58279b1042c288f6e218aee3fbd291d
mury7 <cipriancluj1976@gmail.com>
2018-06-11 14:56:06 +0300

    added test for checking if the "@" is missing in email field.

--- src/test/java/org/fasttrackit/search/LoginTest.java
+++ src/test/java/org/fasttrackit/search/LoginTest.java
@@ -2,12 +2,15 @@ package org.fasttrackit.search;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LoginTest {
@@ -97,6 +100,34 @@ public class LoginTest {
             driver.findElement(By.id("advice-required-entry-email"));
             WebElement errorMsgContainer = driver.findElement(By.className("validation-advice"));
             assertThat("Error message not displayed", errorMsgContainer.isDisplayed());
+            driver.quit();
+        }
+
+    }
+
+    @Test
+    public void loginTestWithoutAtInEmail(){
+        System.setProperty("webdriver.chrome.driver",
+        "C:\\Users\\mury7\\FastTrackIt\\madisonIslandTestByMury\\src\\test\\resources\\drivers\\chromedriver.exe");
+
+
+        WebDriver driver = new ChromeDriver(); {
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+            driver.get("https://fasttrackit.org/selenium-test/");
+            driver.findElement(By.linkText("ACCOUNT")).click();
+            driver.findElement(By.linkText("Log In")).click();
+            driver.findElement(By.name("login[username]")).sendKeys("mailmail.com");
+            driver.findElement(By.name("login[password]")).sendKeys("123456");
+            driver.findElement(By.cssSelector("button[title='Login']")).click();
+
+            JavascriptExecutor js = (JavascriptExecutor)driver;
+
+            WebElement field = driver.findElement(By.id("email"));
+            Boolean is_valid = (Boolean)js.executeScript("return arguments[0].checkValidity();", field);
+            String message = (String)js.executeScript("return arguments[0].validationMessage;", field);
+
+            assertThat("The email field validates email without @ !", is_valid, is(false));
             driver.quit();
         }
 
